dde2c62db4b9aecfae18503c916eaec32ae69615
Luca De Luigi <lucadeluigi91@gmail.com>
2018-11-10 11:26:10 +0100

    added altitude_server.py and vspeed_server.py

---
+++ vspeed_server.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from collections import deque
+import socket
+import json
+import sys
+
+HOST                = ''
+PORT                = 56789
+SOCKET_BUFFER_SIZE  = 1024
+MAX_DEQUE_SIZE      = 100
+XRANGE              = (0, MAX_DEQUE_SIZE)
+YRANGE              = (-10, 10)
+REFRESH_PERIOD      = 1     # millis
+
+server_socket       = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_connection   = ''
+client_addr         = ''
+data                = {}
+
+def data_server():
+    while True:
+        json_msg = client_connection.recv(SOCKET_BUFFER_SIZE)
+        msg = json.loads(json_msg.decode())
+        label, value = msg['label'], msg['value']
+        if label not in data:
+            data[label] = deque([0 for _ in range(MAX_DEQUE_SIZE)], MAX_DEQUE_SIZE)
+        data[label].append(value)
+        client_connection.send('#ACK#'.encode())
+        yield data
+
+lines = {}
+fig = plt.figure(num='Vertical Speed')
+plt.xlim(XRANGE)
+plt.ylim(YRANGE)
+
+def animate(data):
+    for label in data:
+        if label not in lines:
+            new_line, = plt.plot(data[label], label=label)
+            lines[label] = new_line
+            plt.legend(loc='upper right')
+        else:
+            lines[label].set_ydata(data[label])
+    return  [lines[label] for label in lines]
+
+try:
+    with server_socket:
+        print('Socket created')
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(1)
+        print('Waiting connection...')
+        client_connection, client_addr = server_socket.accept()
+        print('Accepted connection from', client_addr)
+        with client_connection:
+            anim = animation.FuncAnimation(fig, animate, data_server, interval=REFRESH_PERIOD, blit=True)
+            plt.show()
+except:
+    print('Exception:', sys.exc_info()[0], '-', sys.exc_info()[1])
+
