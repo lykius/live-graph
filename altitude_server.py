86dce26454238f9d3f33a4e09db69ae87b174b04
Luca De Luigi <lucadeluigi91@gmail.com>
2018-11-19 00:42:25 +0100

    pep8 compliance

--- altitude_server.py
+++ altitude_server.py
@@ -5,18 +5,19 @@ import socket
 import json
 import sys
 
-HOST                = ''
-PORT                = 12345
-SOCKET_BUFFER_SIZE  = 1024
-MAX_DEQUE_SIZE      = 100
-XRANGE              = (0, MAX_DEQUE_SIZE)
-YRANGE              = (40, 70)
-REFRESH_PERIOD      = 1     # millis
+HOST = ''
+PORT = 12345
+SOCKET_BUFFER_SIZE = 1024
+MAX_DEQUE_SIZE = 100
+XRANGE = (0, MAX_DEQUE_SIZE)
+YRANGE = (40, 70)
+REFRESH_PERIOD = 1  # millis
 
-server_socket       = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_connection   = ''
-client_addr         = ''
-data                = {}
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_connection = ''
+client_addr = ''
+data = {}
+
 
 def data_server():
     while True:
@@ -24,15 +25,20 @@ def data_server():
         msg = json.loads(json_msg.decode())
         label, value = msg['label'], msg['value']
         if label not in data:
-            data[label] = deque([0 for _ in range(MAX_DEQUE_SIZE)], MAX_DEQUE_SIZE)
+            data[label] = deque(
+                [0 for _ in range(MAX_DEQUE_SIZE)],
+                MAX_DEQUE_SIZE
+                )
         data[label].append(value)
         client_connection.send('#ACK#'.encode())
         yield data
+
 
 lines = {}
 fig = plt.figure(num='Altitude')
 plt.xlim(XRANGE)
 plt.ylim(YRANGE)
+
 
 def animate(data):
     for label in data:
@@ -42,7 +48,8 @@ def animate(data):
             plt.legend(loc='upper right')
         else:
             lines[label].set_ydata(data[label])
-    return  [lines[label] for label in lines]
+    return [lines[label] for label in lines]
+
 
 try:
     with server_socket:
@@ -53,8 +60,13 @@ try:
         client_connection, client_addr = server_socket.accept()
         print('Accepted connection from', client_addr)
         with client_connection:
-            anim = animation.FuncAnimation(fig, animate, data_server, interval=REFRESH_PERIOD, blit=True)
+            anim = animation.FuncAnimation(
+                fig,
+                animate,
+                data_server,
+                interval=REFRESH_PERIOD,
+                blit=True
+                )
             plt.show()
 except:
     print('Exception:', sys.exc_info()[0], '-', sys.exc_info()[1])
-
--- client.py
+++ client.py
@@ -4,9 +4,9 @@ import json
 import sys
 import random
 
-SERVER_HOST         = '127.0.0.1'
-SERVER_PORT         = 12345
-SOCKET_BUFFER_SIZE  = 1024
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 12345
+SOCKET_BUFFER_SIZE = 1024
 
 data = {}
 data['data1'] = 5
@@ -23,10 +23,12 @@ try:
         print('Connected')
         while True:
             for label in data:
-                msg = {'label': label, 'value': data[label] + (random.random() * 6 - 3)}
+                msg = {
+                    'label': label,
+                    'value': data[label] + (random.random() * 6 - 3)
+                    }
                 json_msg = json.dumps(msg)
                 s.send(json_msg.encode())
                 ack = s.recv(SOCKET_BUFFER_SIZE)
 except:
     print('Exception:', sys.exc_info()[0], '-', sys.exc_info()[1])
-
